429d9f45ee164a264cfd5ae317e28a3e0785d45e
PhanTien05 <PhanTien05>
2025-09-24 23:24:04 +0700

    Tao trang cap nhat lien he

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+//import ContactAdd from "@/views/ContactAdd.vue"; // Import ContactAdd
+
 const routes = [
     {
         path: "/",
@@ -7,15 +9,26 @@ const routes = [
         component: ContactBook,
     },
     {
-        path: "/:pathMatch(.*)*",
-        name: "NotFound",
-        component: () => import("@/views/NotFound.vue")
-    }
+        path: "/:pathMatch(.*)*", // Bắt mọi đường dẫn không khớp
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true, // Truyền các biến trong $route.params vào làm props
+    },
+    /* {
+         path: "/contacts/add",
+         name: "contact.add",
+         component: ContactAdd, // Sử dụng component ContactAdd
+     },*/
 ];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
-
 
 export default router;
